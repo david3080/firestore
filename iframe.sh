9674aba07a57e47b5dc5cebe0d4dfe4fb7316eba
david3080 <david3080@gmail.com>
2020-08-16 06:03:17 +0900

    commit

--- iframe.sh
+++ iframe.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Youtube動画を追加する
 cat index.html | sed 's#<link rel="stylesheet" href="https://storage.googleapis.com/codelab-elements/codelab-elements.css">#<link rel="stylesheet" href="./codelab-elements.css">#g' > index.html.1
 mv index.html.1 index.html
 cat index.html | sed 's#<p>v_hR4K4auoQ</p>#<br><iframe width="560" height="315" src="https://www.youtube.com/embed/v_hR4K4auoQ?cc_load_policy=1&amp;cc_lang_pref=ja" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>#g' > index.html.1
@@ -25,6 +26,16 @@ mv index.html.1 index.html
 cat index.html | sed 's#<p>rERRuBjxJ80</p>#<br><iframe width="560" height="315" src="https://www.youtube.com/embed/rERRuBjxJ80?cc_load_policy=1&amp;cc_lang_pref=ja" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>#g' > index.html.1
 mv index.html.1 index.html
 cat index.html | sed 's#<p>77XmRDtOL7c</p>#<br><iframe width="560" height="315" src="https://www.youtube.com/embed/77XmRDtOL7c?cc_load_policy=1&amp;cc_lang_pref=ja" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>#g' > index.html.1
+mv index.html.1 index.html
+
+# JSONエディタを追加する
+cat index.html | sed 's#<p>json01</p>#<iframe width="820" height="220" src="./json01.html" frameborder="1"></iframe>#g' > index.html.1
+mv index.html.1 index.html
+cat index.html | sed 's#<p>json02</p>#<iframe width="820" height="420" src="./json02.html" frameborder="1"></iframe>#g' > index.html.1
+mv index.html.1 index.html
+cat index.html | sed 's#<p>json03</p>#<iframe width="820" height="420" src="./json03.html" frameborder="1"></iframe>#g' > index.html.1
+mv index.html.1 index.html
+cat index.html | sed 's#<p>json04</p>#<iframe width="820" height="620" src="./json04.html" frameborder="1"></iframe>#g' > index.html.1
 mv index.html.1 index.html
 
 exit 0
