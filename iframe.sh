f9ca89e2f06d79b8011b6050b4b776c7804a19e3
david3080 <david3080@gmail.com>
2020-08-29 15:06:04 +0900

    commit

--- iframe.sh
+++ iframe.sh
@@ -52,5 +52,9 @@ cat index.html | sed 's#<p>dartpad02</p>#<iframe width="850" height="500" src="h
 mv index.html.1 index.html
 cat index.html | sed 's#<p>dartpad03</p>#<iframe width="850" height="500" src="https://dartpad.dev/embed-inline.html?id=15c1b361894b2462d4e118b1c96e54a8" frameborder="1"></iframe>#g' > index.html.1
 mv index.html.1 index.html
+cat index.html | sed 's#<p>dartpad04</p>#<iframe width="850" height="500" src="https://dartpad.dev/embed-inline.html?id=6a0336662fa73c384c4b8d7352e5bca3" frameborder="1"></iframe>#g' > index.html.1
+mv index.html.1 index.html
+cat index.html | sed 's#<p>dartpad05</p>#<iframe width="850" height="500" src="https://dartpad.dev/embed-inline.html?id=b1f2329101137e1ab19cc77b80df47c6" frameborder="1"></iframe>#g' > index.html.1
+mv index.html.1 index.html
 
 exit 0
